3cdfece3439adb2d50a993c39ba02873cb2c39c8
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-07-05 13:05:25 -0700

    add lockedboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+#!/usr.bin/python3
+"""
+0-lockedboxes module
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be opened
+    """
+    num_boxes = len(boxes)
+    unlocked = [False] * num_boxes
+    unlocked[0] = True
+    keys = boxes[0][:]
+
+    while keys:
+        key = keys.pop()
+        if not unlocked[key]:
+            unlocked[key] = True
+            new_keys = boxes[key]
+            keys.extend(new_keys)
+
+    return all(unlocked)
