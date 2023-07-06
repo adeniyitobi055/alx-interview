52fa42dae3abc4a04f2630b00d0ff3514b30b9ad
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-07-06 01:58:39 -0700

    update 0-lockedboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -11,11 +11,10 @@ def canUnlockAll(boxes):
     num_boxes = len(boxes)
     unlocked = [False] * num_boxes
     unlocked[0] = True
-    keys = boxes[0][:]
+    keys = boxes[0]
 
-    while keys:
-        key = keys.pop()
-        if not unlocked[key]:
+    for key in keys:
+        if key < num_boxes and not unlocked[key]:
             unlocked[key] = True
             new_keys = boxes[key]
             keys.extend(new_keys)
