2d5954ae6e4c244163b098cd419bdf05df2f47f8
adeniyitobi055 <61655996+adeniyitobi055@users.noreply.github.com>
2023-08-15 13:08:26 +0100

    Create 0-rotate_2d_matrix.py

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+rotate_2d_matrix module
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """
+    rotates a 2d matrix 90 degrees clockwise
+
+    Args:
+        matrix: A 2d list of lists.
+    
+    Rotates the matrix in place
+    """
+
+    n = len(matrix)
+
+    for layer in range(n // 2):
+        first = layer
+        last = n - layer - 1
+
+        for i in range(first, last):
+            temp = matrix[first][i]
+            matrix[first][i] = matrix[last - i][first]
+            matrix[last - i][first] = matrix[last][last - i]
+            matrix[last][last - i] = matrix[i][last]
+            matrix[i][last] = temp
