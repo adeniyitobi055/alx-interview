44d16e12ee1daa4e81c25692c59b87c60ec08917
adeniyitobi055 <61655996+adeniyitobi055@users.noreply.github.com>
2023-09-05 10:28:57 +0100

    Update is_prime.py

--- 0x0A-primegame/is_prime.py
+++ 0x0A-primegame/is_prime.py
@@ -16,7 +16,7 @@ def is_prime(n):
             return False
         
         i = 5
-        while i * i <= 5:
+        while i * i <= n:
             if n % i == 0 or n % (i + 2) == 0:
                 return False
             i += 6
