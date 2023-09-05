a310165920168273975af09793e633ff2336cf8d
adeniyitobi055 <61655996+adeniyitobi055@users.noreply.github.com>
2023-09-05 10:20:04 +0100

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,6 +2,7 @@
 """
 0-prime_game module
 """
+count_prime = __import__('count_prime').count_prime
 
 
 def isWinner(x, nums):
@@ -15,35 +16,6 @@ def isWinner(x, nums):
         the name of the player that won the most rounds,
         if the winner cannot be determined, None is returned.
     """
-
-    def is_prime(n):
-        """
-        checks if a given number `n` is prime
-        """
-        if n <= 1:
-            return False
-        if n <= 3:
-            return True
-        if n % 2 == 0 or n % 3 == 0:
-            return False
-        
-        i = 5
-        while i * i <= 5:
-            if n % i == 0 or n % (i + 2) == 0:
-                return False
-            i += 6
-        return True
-    
-    def count_prime(n):
-        """
-        count number from `2` to `n` to check if they are
-        prime number using `is_prime` function
-        """
-        count = 0
-        for i in range(2, n + 1):
-            if is_prime(i):
-                count += 1
-        return count
     
     maria_wins = 0
     ben_wins = 0
